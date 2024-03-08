381fc0ec2de8063ef447d2215fda4bf71f57db58
BBalintt <balinth03@gmail.com>
2024-03-08 16:09:43 +0100

    ds

--- script.js
+++ script.js
@@ -34,4 +34,12 @@ function openModal() {
     document.getElementById("myModal").style.display = "none";
   }
 
+  function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+  }
+  
+  function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+  }
+
   
